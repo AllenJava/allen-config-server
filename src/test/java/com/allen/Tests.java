7410c5a2e4ce790cface56e9862577fe4b1e80dc
liqiaochen <liqiaochen@infinite.net.cn>
2018-05-16 11:20:28 +0800

    代码更新

--- src/test/java/com/allen/Tests.java
+++ src/test/java/com/allen/Tests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AllenSpringcloudConfigApplicationTests {
+public class Tests {
 
 	@Test
 	public void contextLoads() {
